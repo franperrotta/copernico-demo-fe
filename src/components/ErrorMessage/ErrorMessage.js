189451d8469e8aa860e0e51657901dd9247d88fe
Francisco <francisco.perrotta@wetek.io>
2020-10-14 12:36:07 -0300

    Changes: deleted status code

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -4,19 +4,16 @@ export default function ErrorMessage(props) {
     if(props.error) {
         return (
             <div className="alert alert-danger errorMessage" role="alert">
-                { props.error.statusCode } <br/>
                 { 
                     props.error.statusCode === 404 
                         ? props.error.message.includes('No existe un objeto asociado para el sujeto')
                             ? props.error.message
                             : props.error.message.includes('Endpoint "GET /sujetos//objetos/obligaciones" not found.') 
-                                ? 'Endpoint en servidor no encontrado'
+                                ? 'Endpoint {/sujetos//objetos/obligaciones} no encontrado.'
                                 : `El sujeto asociado al cuit ingresado no se encuentra en la base de datos.`
                         : props.error.statusCode === 500 
                             ? `El servidor no responde.`
-                            : props.error.statusCode === 400
-                                ? `No se encuentra el servidor.` 
-                                : props.error.message
+                            : props.error.message
                 }
             </div>
         );
